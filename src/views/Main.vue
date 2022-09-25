5ec323a326cc8dbf2c2dc6a9e898e1c52d408b4f
YohaLin <kate1995y@gmail.com>
2022-09-25 21:51:20 +0800

    try bootstrap-vue

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -83,9 +83,9 @@
     <div>
   <b-button v-b-modal.modal-1>Launch demo modal</b-button>
 
-  <b-modal id="modal-1" title="BootstrapVue">
+  <!-- <b-modal id="modal-1" title="BootstrapVue">
     <p class="my-4">Hello from modal!</p>
-  </b-modal>
+  </b-modal> -->
 </div>
   </div>
 </template>
